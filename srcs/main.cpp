70fcf60540050ec3b5c7b14d370950b9b67b4855
David Goubin <dgoubin@k0r3p11.42mulhouse.fr>
2024-04-08 17:16:38 +0200

    un peu de mise en forme

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -11,28 +11,35 @@ int main( int ac, char *av[] ) {
         return 0;
     }
 
-
+    /* transforme av[1] (le port des arguments) en int */
     std::istringstream is(av[1]);
     int port;
     is >> port;
+
     int connexionFd;
     int socketFd = socket(AF_INET, SOCK_STREAM, 0); /* creer un socket */
     struct sockaddr_in addr_serveur; /* creer la structure des donnees du socket du serveur */
-    bzero(&addr_serveur, sizeof(addr_serveur));
-    addr_serveur.sin_family = AF_INET;
+    bzero(&addr_serveur, sizeof(addr_serveur)); /* on set tout a 0 pour etre sur */
+    addr_serveur.sin_family = AF_INET; /* pas encore comprit a quoi ca correspondait exactement AF_INET mais c'est utilise tout le temps en reseau */
     addr_serveur.sin_port = htons(port); /* Port du serveur */
     addr_serveur.sin_addr.s_addr = htonl(INADDR_ANY); /* adresse IP de la machine */
-    if (bind(socketFd, (struct sockaddr *) &addr_serveur, sizeof(addr_serveur)) < 0) { /* lie le socket avec la structure */
+
+    /* lie le socket avec la structure */
+    if (bind(socketFd, (struct sockaddr *) &addr_serveur, sizeof(addr_serveur)) < 0) {
         std::cout << "Binding \033[31mfailed\033[0m" << std::endl;
         return 0;
     }
     std::cout << "Server \033[33mlaunched\033[0m on port " << av[1] << std::endl;
-    if (listen(socketFd, 10) < 0) { /* creer une longueur max a la file d attente des demandes de connexion */
+
+    /* creer une longueur max a la file d attente des demandes de connexion */
+    if (listen(socketFd, 10) < 0) {
         std::cout << "Listening \033[31mfailed\033[0m" << std::endl;
         return 0;
     }
     std::cout << "Server \033[33mlistening\033[0m on port " << av[1] << std::endl;
-    if ((connexionFd = accept(socketFd, NULL, NULL)) < 0) { /* fonction bloquante qui attend une connexion (renvoie le fd du nouveau socket) */
+
+    /* fonction bloquante qui attend une connexion (renvoie le fd du nouveau socket) */
+    if ((connexionFd = accept(socketFd, NULL, NULL)) < 0) {
         std::cout << "Connexion \033[31mfailed\033[0m" << std::endl;
         return 0;
     }
