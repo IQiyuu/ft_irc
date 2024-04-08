468d140917135c8c9134007edfb88a26b6c40ffb
David Goubin <dgoubin@k0r3p11.42mulhouse.fr>
2024-04-08 17:14:25 +0200

    des couleurs car c'est fun pour se repere (succcess, failed, status => luanching, binding...) + le port est par rapport au av mtn

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <sstream>
 
 int main( int ac, char *av[] ) {
     if (ac != 3) {
@@ -11,8 +12,9 @@ int main( int ac, char *av[] ) {
     }
 
 
-
-    int port = 6666;
+    std::istringstream is(av[1]);
+    int port;
+    is >> port;
     int connexionFd;
     int socketFd = socket(AF_INET, SOCK_STREAM, 0); /* creer un socket */
     struct sockaddr_in addr_serveur; /* creer la structure des donnees du socket du serveur */
@@ -21,19 +23,19 @@ int main( int ac, char *av[] ) {
     addr_serveur.sin_port = htons(port); /* Port du serveur */
     addr_serveur.sin_addr.s_addr = htonl(INADDR_ANY); /* adresse IP de la machine */
     if (bind(socketFd, (struct sockaddr *) &addr_serveur, sizeof(addr_serveur)) < 0) { /* lie le socket avec la structure */
-        std::cout << "Binding failed" << std::endl;
+        std::cout << "Binding \033[31mfailed\033[0m" << std::endl;
         return 0;
     }
-    std::cout << "Server launched on port " << av[1] << std::endl;
+    std::cout << "Server \033[33mlaunched\033[0m on port " << av[1] << std::endl;
     if (listen(socketFd, 10) < 0) { /* creer une longueur max a la file d attente des demandes de connexion */
-        std::cout << "Listening failed" << std::endl;
+        std::cout << "Listening \033[31mfailed\033[0m" << std::endl;
         return 0;
     }
-    std::cout << "Server listening on port " << av[1] << std::endl;
+    std::cout << "Server \033[33mlistening\033[0m on port " << av[1] << std::endl;
     if ((connexionFd = accept(socketFd, NULL, NULL)) < 0) { /* fonction bloquante qui attend une connexion (renvoie le fd du nouveau socket) */
-        std::cout << "Connexion failed" << std::endl;
+        std::cout << "Connexion \033[31mfailed\033[0m" << std::endl;
         return 0;
     }
-    std::cout << "Connexion successful on port " << av[1] << std::endl;
+    std::cout << "Connexion \033[32msuccessful\033[0m on port " << av[1] << std::endl;
     return 0;
 }
